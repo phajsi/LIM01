eb6e151601d95d2d1bea2c3e42c42f896f5b2360
simenO <42930807+simenO@users.noreply.github.com>
2021-02-26 14:07:29 +0100

    connected create exercise frontend to backend api

--- frontend/src/components/CreateForstaelse.js
+++ frontend/src/components/CreateForstaelse.js
@@ -1,6 +1,4 @@
-/* eslint-disable react/no-unknown-property */
-/* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Button,
   FormControlLabel,
@@ -10,6 +8,7 @@ import {
   TextField,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,20 +27,62 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const axiosInstance = axios.create({
+  baseURL: `${process.env.REACT_APP_API_URL}/api/`,
+  timeout: 5000,
+  headers: {
+    // eslint-disable-next-line prettier/prettier
+    'Authorization': `JWT ${localStorage.getItem('access')}`,
+    'Content-Type': 'application/json',
+    // eslint-disable-next-line prettier/prettier
+    'accept': 'application/json',
+  },
+});
+
 const CreateForstaelse = () => {
   const classes = useStyles();
+
+  const [formData, setFormData] = useState({
+    chat: '',
+    question: '',
+    answer: null,
+    explanation: '',
+  });
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onClick = (e) => {
+    e.preventDefault();
+    axiosInstance
+      .post('/forstaelse/', {
+        chat: formData.chat,
+        question: formData.question,
+        answer: formData.answer,
+        explanation: formData.explanation,
+      })
+      .then((response) => {
+        return response;
+      })
+      .catch((e) => {
+        return e;
+      });
+  };
+  const handleRadio = (event) =>
+    setFormData({ ...formData, answer: event.target.value });
 
   return (
     <Paper className={classes.root}>
       <h1>Create Forståelse</h1>
       <form className={classes.form}>
-        <p>Skriv textmeldingen her: </p>
+        <p>Skriv tekstmeldingen her: </p>
         <TextField
           name="chat"
           multiline
           fullWidth
           rowsMax={3}
           variant="outlined"
+          onChange={(e) => onChange(e)}
         />
         <p>Skriv spørsmålet her: </p>
         <TextField
@@ -50,11 +91,12 @@ const CreateForstaelse = () => {
           fullWidth
           rowsMax={3}
           variant="outlined"
+          onChange={(e) => onChange(e)}
         />
         <p>Velg svaret som er riktig </p>
-        <RadioGroup row>
-          <FormControlLabel value="JA" control={<Radio />} label="JA" />
-          <FormControlLabel value="NEI" control={<Radio />} label="NEI" />
+        <RadioGroup row onChange={handleRadio}>
+          <FormControlLabel value="true" control={<Radio />} label="JA" />
+          <FormControlLabel value="false" control={<Radio />} label="NEI" />
         </RadioGroup>
         <p>Skriv forklaring her: </p>
         <TextField
@@ -63,10 +105,14 @@ const CreateForstaelse = () => {
           fullWidth
           rowsMax={3}
           variant="outlined"
+          onChange={(e) => onChange(e)}
         />
       </form>
+      <Button onClick={(e) => onClick(e)} variant="contained">
+        OPPRETT
+      </Button>
       <Button variant="contained">AVBRYT</Button>
-      <Button variant="contained">OPPRETT</Button>
+      <Button variant="contained">PREVIEW</Button>
     </Paper>
   );
 };
