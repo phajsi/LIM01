e98cacfe70967c72c966a5cf91394866cceb0c60
Phajsi Choque Halvorsen <phajsich@stud.ntnu.no>
2021-02-27 11:42:13 +0100

    Refactured style code into its own file

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,26 +1,14 @@
 import React, { useState, Fragment } from 'react';
 import { Redirect, Link } from 'react-router-dom';
 
-import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { connect } from 'react-redux';
-import { logout } from '../actions/auth';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+import { logout } from '../../actions/auth';
+import useStyles from './styles';
 
 const Navbar = ({ logout, isAuthenticated }) => {
   const [redirect, setRedirect] = useState(false);
---
+++ frontend/src/containers/CreateExercises/styles.js
@@ -0,0 +1,15 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: '#f5f5f5',
+    maxWidth: theme.spacing(40),
+    marginTop: theme.spacing(3),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    margin: 'auto',
+  },
+}));
+
+export default useStyles;
