597965c0a869ded761157704beae8f515e2f1cfb
Simen <sommer195@yahoo.com>
2021-03-27 18:52:18 +0100

    removed routes to unused paths in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,11 +10,8 @@ import ResetPasswordConfirm from './containers/ResetPasswordConfirm';
 import StartPage from './containers/StartPage';
 import store from './store';
 import Layout from './hocs/Layout';
-import Forstaelse from './containers/Forstaelse/Forstaelse';
 import CreateExercises from './containers/CreateExercises/CreateExercises';
-import Chat from './containers/Chat/Chat';
 import PlaySets from './containers/PlaySets';
-import RyddeSetninger from './containers/RyddeSetninger/RyddeSetninger';
 
 const App = () => (
   <Provider store={store}>
@@ -23,13 +20,10 @@ const App = () => (
         <Route exact path="/" component={StartPage} />
         <Layout>
           <Route exact path="/home" component={Home} />
-          <Route exact path="/forstaelse" component={Forstaelse} />
           <Route exact path="/createexercise" component={CreateExercises} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
           <Route exact path="/reset-password" component={ResetPassword} />
-          <Route exact path="/chat" component={Chat} />
-          <Route exact path="/rydde-setninger" component={RyddeSetninger} />
           <Route exact path="/sets" component={PlaySets} />
           <Route
             exact
