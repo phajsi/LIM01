fc1b64a36ad7e96f98c021cdfb62cb71a6ad2c3b
phajsi <23497392+phajsi@users.noreply.github.com>
2021-04-05 21:45:52 +0200

    Minor edits

--- frontend/src/components/Navbar/styles.js
+++ frontend/src/components/Navbar/styles.js
@@ -1,7 +1,9 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-  root: {},
+  root: {
+    background: 'linear-gradient(90deg, #53A77A 1.46%, #80D197 100%)',
+  },
   menuButton: {
     marginRight: theme.spacing(2),
   },
