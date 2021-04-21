57155cfeb3c23c9b785401b0d214a229fecd94f3
phajsi <23497392+phajsi@users.noreply.github.com>
2021-04-21 16:19:42 +0200

    removed unnecessary brackets

--- frontend/src/containers/Chat/Chat.js
+++ frontend/src/containers/Chat/Chat.js
@@ -127,9 +127,7 @@ const Chat = ({
   function fireAudio() {
     setDisabled(true);
     playAudio(chataudio);
-    setTimeout(() => {
-      setDisabled(false);
-    }, 6000);
+    setTimeout(() => setDisabled(false), 6000);
   }
 
   useEffect(() => {
