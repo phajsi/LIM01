eefe7f6248e7595dd6833c4fc5e1a1fd66f46058
phajsi <23497392+phajsi@users.noreply.github.com>
2021-04-22 10:41:10 +0200

    Changed projects email to dipickle.lim@gmail.com

--- backend/lim_backend/settings.py
+++ backend/lim_backend/settings.py
@@ -92,8 +92,8 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'simenfop@gmail.com'
-EMAIL_HOST_PASSWORD = 'lcjilhfghkezgphv'
+EMAIL_HOST_USER = 'dipickle.lim@gmail.com'
+EMAIL_HOST_PASSWORD = 'crdpcvrpitchpjtn'
 EMAIL_USE_TLS = True
 
 
